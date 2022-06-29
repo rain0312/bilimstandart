fd7c87f95dd677d057108828b8f1ec4eabe4952f
Stanislav Lee <sstanislavv.li@gmail.com>
2022-06-29 18:29:24 +0600

    joj

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,23 +4,25 @@ import emailjs from 'emailjs-com'
 const initialState = {
   name: '',
   email: '',
+  date: '',
+  phone: '',
   message: '',
 }
 export const Contact = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState)
+  const [{ name, email, message, phone, date, show }, setState] = useState(initialState)
 
   const handleChange = (e) => {
     const { name, value } = e.target
-    setState((prevState) => ({ ...prevState, [name]: value }))
+      setState((prevState) => ({ ...prevState, [name]: value }))
   }
-  const clearState = () => setState({ ...initialState })
+
+  const clearState = () => setState({ ...initialState, [show]: true })
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(name, email, message)
     emailjs
       .sendForm(
-        'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID'
+        'service_ewdezvl', 'template_je3ymic', e.target, 'zhkox06-88sllS1Km'
       )
       .then(
         (result) => {
@@ -36,12 +38,102 @@ export const Contact = (props) => {
     <div>
       <div id='contact'>
         <div className='container'>
+          <div className='col-md-8'>
+            <div className='row'>
+              <div className='section-title'>
+                <h2>Связаться с нами</h2>
+                <p>
+                  Пожалуйста введите свои контактные данные и нужную информацию, а мы свяжемся с вами в ближайшее время
+                </p>
+              </div>
+              <form name='sentMessage' validate onSubmit={handleSubmit}>
+                <div className='row'>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='text'
+                        id='name'
+                        name='name'
+                        className='form-control'
+                        placeholder='ФИО'
+                        required
+                        value={name}
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='email'
+                        id='email'
+                        name='email'
+                        className='form-control'
+                        placeholder='Email'
+                        value={email}
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='date'
+                        id='date'
+                        name='date'
+                        value={date}
+                        className='form-control'
+                        placeholder='Дата'
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                  <div className='col-md-6'>
+                    <div className='form-group'>
+                      <input
+                        type='phone'
+                        id='phone'
+                        name='phone'
+                        className='form-control'
+                        placeholder='Телефон'
+                        required
+                        value={phone}
+                        onChange={e => setState((prevState) => ({ ...prevState, [e.target.name]: e.target.value.replace(/[^0-9]/g, "") }))}
+                        />
+                      <p className='help-block text-danger'></p>
+                    </div>
+                  </div>
+                </div>
+                <div className='form-group'>
+                  <textarea
+                    name='message'
+                    id='message'
+                    className='form-control'
+                    rows='4'
+                    value={message}
+                    placeholder='Наименование учебного заведения и доп. информация'
+                    required
+                    onChange={handleChange}
+                  ></textarea>
+                </div>
+                <div id='success'></div>
+                <button type='submit' className='btn btn-custom btn-lg'>
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
           <div className='col-md-3 col-md-offset-1 contact-info'>
             <div className='contact-item'>
-              <h3>Контакты</h3>
+              <h3>Contact Info</h3>
               <p>
                 <span>
-                  <i className='fa fa-map-marker'></i> Адрес
+                  <i className='fa fa-map-marker'></i> Address
                 </span>
                 {props.data ? props.data.address : 'loading'}
               </p>
@@ -49,7 +141,7 @@ export const Contact = (props) => {
             <div className='contact-item'>
               <p>
                 <span>
-                  <i className='fa fa-phone'></i> Телефон
+                  <i className='fa fa-phone'></i> Phone
                 </span>{' '}
                 {props.data ? props.data.phone : 'loading'}
               </p>
@@ -78,7 +170,11 @@ export const Contact = (props) => {
           </div>
         </div>
       </div>
+      <div id='footer'>
+        <div className='container text-center'>
 
+        </div>
+      </div>
     </div>
   )
 }
